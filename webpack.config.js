0c1919e0d0bbe924e664f06a9d5cb1499bc9ac8f
rolan <roland.alex@mail.ru>
2021-03-07 01:14:23 +0400

    add title layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/main.js',
+  mode: 'development',
+  entry: ['./src/index.js', './src/scss/style.scss'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
@@ -10,5 +11,43 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     watchContentBase: true,
-  }
+    open: true
+  },
+  module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+					presets: ['@babel/preset-env'],
+					}
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'css/[name].css',
+						}
+					},
+					{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'css-loader?-url'
+					},
+					{
+						loader: 'postcss-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+		],
+	}
 };
